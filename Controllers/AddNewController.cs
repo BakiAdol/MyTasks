256ae4b836b955745996df25e7f9251c458fc184
BakiAdol <joyaadol@gmail.comn>
2022-08-30 13:40:59 +0600

    addnew controller and view created and design

---
+++ Controllers/AddNewController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyTasks.Controllers
+{
+    public class AddNewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
