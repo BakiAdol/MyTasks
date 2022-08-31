22310567fe8d90a4970d700e4499183944259e88
BakiAdol <joyaadol@gmail.comn>
2022-08-31 15:28:23 +0600

    Details controller created

---
+++ Controllers/DetailsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyTasks.Services;
+using System.Threading.Tasks;
+
+namespace MyTasks.Controllers
+{
+    public class DetailsController : Controller
+    {
+        #region Props
+        private readonly IMyTaskService myTaskService;
+        #endregion
+
+        #region Ctor
+        public DetailsController(IMyTaskService myTaskService)
+        {
+            this.myTaskService = myTaskService;
+        }
+        #endregion
+
+        public IActionResult Index(int taskId)
+        {
+            return View();
+        }
+    }
+}
