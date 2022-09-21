fda8e1d9a7e1c8cf768798e296d19c1d158dcfe8
BakiAdol <joyaadol@gmail.comn>
2022-09-21 15:12:44 +0600

    log, reg and logout functionality created

--- MyTasks/Controllers/AccountController.cs
+++ MyTasks/Controllers/AccountController.cs
@@ -1,10 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using MyTasksClassLib.Models;
+using System.Security.Cryptography.Xml;
 
 namespace MyTasks.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, 
+            SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
         public IActionResult Index()
         {
             return View();
@@ -16,19 +27,60 @@ namespace MyTasks.Controllers
             return View(new RegisterModel());
         }
         [HttpPost]
-        public IActionResult Registration(RegisterModel regInfo)
+        public async Task<IActionResult> Registration(RegisterModel regInfo)
         {
-            return View(new RegisterModel());
+            var user = new UserModel { UserName = regInfo.Email, Email = regInfo.Email, 
+                Name = regInfo.Name};
+
+            var res = await _userManager.CreateAsync(user, regInfo.Password);
+
+            if(res.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, isPersistent: false);
+
+                return RedirectToAction("Index", "Tasks");
+            }
+
+            AddError(res);
+
+            return View(regInfo);
         }
+       
         [HttpGet]
         public IActionResult Login()
         {
             return View(new LoginModel());
         }
         [HttpPost]
-        public IActionResult Login(LoginModel loginInfo)
+        public async Task<IActionResult> Login(LoginModel loginInfo)
         {
-            return View(new LoginModel());
+            var res = await _signInManager.PasswordSignInAsync(loginInfo.Email, loginInfo.Password,
+                loginInfo.RememberMe, lockoutOnFailure: false);
+
+            if(res.Succeeded)
+            {
+                return RedirectToAction("Index", "Tasks");
+            }
+
+            ModelState.AddModelError(string.Empty, "Invalid login attempt");
+
+            return View(loginInfo);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> LogOut()
+        {
+            await _signInManager.SignOutAsync();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        private void AddError(IdentityResult res) 
+        {
+            foreach(var error in res.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
         }
     }
 }
