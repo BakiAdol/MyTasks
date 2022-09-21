0623f09b0ae8ebba556c8f20d0f5ca6a95eb87af
BakiAdol <joyaadol@gmail.comn>
2022-09-21 13:04:48 +0600

    login, reg model and view and account controller created

---
+++ MyTasks/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyTasksClassLib.Models;
+
+namespace MyTasks.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Registration()
+        {
+            return View(new RegisterModel());
+        }
+        [HttpPost]
+        public IActionResult Registration(RegisterModel regInfo)
+        {
+            return View(new RegisterModel());
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View(new LoginModel());
+        }
+        [HttpPost]
+        public IActionResult Login(LoginModel loginInfo)
+        {
+            return View(new LoginModel());
+        }
+    }
+}
